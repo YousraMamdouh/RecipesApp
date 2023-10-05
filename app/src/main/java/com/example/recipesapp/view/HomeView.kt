4a988965f4bd985d4b59ac289fb90ed364fa2484
YousraMamdouh <yousramamdouh98@gmail.com>
2023-10-05 14:06:13 +0300

    network check

--- app/src/main/java/com/example/recipesapp/view/HomeView.kt
+++ app/src/main/java/com/example/recipesapp/view/HomeView.kt
@@ -1,12 +1,16 @@
 package com.example.recipesapp.view
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesapp.R
 import com.example.recipesapp.model.Repository
+import com.example.recipesapp.network.NetworkChecker
 import com.example.recipesapp.network.RecipesClient
 import com.example.recipesapp.viewModel.HomeViewModel
 import com.example.recipesapp.viewModel.HomeViewModelFactory
@@ -15,20 +19,35 @@ import com.example.recipesapp.viewModel.HomeViewModelFactory
  * Home View of the Recipes App responsible for displaying a list of recipes.
  */
 class HomeView : AppCompatActivity() {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkChecker = NetworkChecker(connectivityManager)
+
     // UI components
     private lateinit var recyclerView: RecyclerView
     private lateinit var factory: HomeViewModelFactory
     private lateinit var viewModel: HomeViewModel
     private lateinit var adapter: RecipesAdapter
     private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var progressBar: ProgressBar
+    private lateinit var noInternetRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recyclerView)
+        progressBar = findViewById(R.id.progressBar)
+        setMyProgressBarVisibility(true)
         createViewModel()
         setUpRecyclerView()
-        observeRecipesLiveData()
+        // Initially, set the "No Internet" RecyclerView to be visible
+        noInternetRecyclerView.visibility = RecyclerView.VISIBLE
+        // Perform a network check and handle visibility based on network availability
+        networkChecker.performAction {
+            // When the network is available, observe recipes LiveData
+            observeRecipesLiveData()
+            // Hide the "No Internet" RecyclerView when the network is available
+            noInternetRecyclerView.visibility = RecyclerView.GONE
+        }
     }
 
     /**
@@ -41,7 +60,6 @@ class HomeView : AppCompatActivity() {
             )
         )
         viewModel = ViewModelProvider(this, factory)[HomeViewModel::class.java]
-
     }
 
     /**
@@ -51,6 +69,7 @@ class HomeView : AppCompatActivity() {
         viewModel.recipes.observe(this) { recipes ->
             if (recipes != null) {
                 adapter.submitList(recipes)
+                setMyProgressBarVisibility(false)
             }
         }
     }
@@ -64,5 +83,17 @@ class HomeView : AppCompatActivity() {
         adapter = RecipesAdapter(this, ArrayList())
         recyclerView.adapter = adapter
         recyclerView.layoutManager = layoutManager
+    }
+
+    /**
+     * Sets the visibility of the progress bar based on the provided boolean value.
+     *
+     * @param visibility True to make the progress bar visible, false to hide it.
+     */
+    private fun setMyProgressBarVisibility(visibility: Boolean) {
+        if (visibility)
+            progressBar.visibility = ProgressBar.VISIBLE
+        else
+            progressBar.visibility = ProgressBar.GONE
     }
 }
